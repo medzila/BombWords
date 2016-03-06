f6afba86ce8d48852eb3ddda9ea76992c1e155af
LIMAM Mohamed <Momo@MacBook-Pro-de-LIMAM.local>
2016-03-06 19:41:27 +0100

    BWS-2-3-4 On peut cliquer pour lancer des "missiles" et les détruire en tapant le mot associé.

--- game.js
+++ game.js
@@ -4,13 +4,17 @@
  * and open the template in the editor.
  */
 
-var canvas, ctx, w, h , canvasT , ctxT, wT, hT,pos,index;
-var motEntree = [];
+var canvas, ctx, w, h; 
+var canvasT , ctxT, wT, hT;
+var pos,index,ind;
+var mots = ["BONJOUR","AUREVOIR","MERCI","MOMO"];
+var motTapee="";
+var missiles = [];
+
 
 window.onload = function(){
     init();
 };
-
 
 function init(){
     console.log("Page chargée");
@@ -25,31 +29,73 @@ function init(){
     ctxT = canvasT.getContext('2d');
     
     pos = 10;
-    
+        
     requestAnimationFrame(mainloop);
     document.addEventListener('keydown',toucheAppuyee,false);
     document.addEventListener('keyup',toucheRelachee,false);
+    document.addEventListener('click',lancementMissile,false);
+
     
 };
 
+function missile() {
+    this.x=Math.random()*w;
+    this.y=0;
+    this.color='red';
+    this.speed=0.5;
+    this.rand = Math.floor(Math.random() * 4);
+    this.motMissile = mots[this.rand];
+    this.move = function(){
+        this.y+=this.speed;
+    };
+    this.draw = function(ctx){
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x,this.y,12*this.motMissile.length,20);
+        ctx.font = "15px Calibri,Geneva,Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText(this.motMissile, this.x,this.y+15);
+        ctx.restore();
+    };
+}
+
 function mainloop(){
+    ctx.clearRect(0, 0, w, h);
+    for(index=0;index < missiles.length;index++){
+        missiles[index].draw(ctx);
+        missiles[index].move();
+    }
     requestAnimationFrame(mainloop);
 }
 
 function toucheAppuyee(evt){
-    console.log("touche appuyee code=" + evt.keyCode);
+    //console.log("touche appuyee code=" + evt.keyCode);
     ctxT.font = "68px Calibri,Geneva,Arial";
     ctxT.fillStyle = "black";
 }
 
 function toucheRelachee(evt){
     ctxT.clearRect(0,0,wT,hT);
-    motEntree.push(evt.keyCode);
-    console.log("touche relachee");
-    index = 0;
-    pos = 10;
-    for	(index = 0; index < motEntree.length; index++) {
-        ctxT.fillText(String.fromCharCode(motEntree[index]),pos, 68);
-        pos+=45;
+    if(evt.keyCode===13){
+        testMotEcrit();
+        motTapee="";
+        ctxT.clearRect(0,0,wT,hT);
+    }else{
+        car = String.fromCharCode(evt.keyCode);
+        ctxT.fillText(car,pos, 68);
+        motTapee+=car;
+    }
+}
+
+function lancementMissile(){
+    missiles.push(new missile());
+}
+
+function testMotEcrit(){
+    for(ind=0;ind < missiles.length;ind++){
+        if(motTapee===missiles[ind].motMissile){
+            missiles.splice(ind,1);
+            break;
+        }
     }
 }
