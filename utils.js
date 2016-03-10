a77444771baf6ad0b6d35b11b479850f470f1ef3
Mehdi Elhajami <mehdi_me@hotmail.fr>
2016-03-10 19:40:20 +0100

    UTILS

---
+++ utils.js
@@ -0,0 +1,21 @@
+/* 
+ * Utils functions
+ */
+ 
+ function distanceBetweenTwoPoints(x1, y1, x2, y2){
+	 console.log("Apl fctn: dist");
+	var a = x1 - x2;
+	var b = y1 - y2;
+	
+	return Math.sqrt(a*a + b*b);
+}
+
+function angleBetweenTwoPoints(x1, y1, x2, y2){
+	var dx = x1 - x2;
+	var dy = y1 - y2;
+	var angle = Math.atan2(dy, dx); 
+	
+	return angle;
+	/*x[i] = xin - cos(angle) * segLength;
+	y[i] = yin - sin(angle) * segLength;*/
+}
